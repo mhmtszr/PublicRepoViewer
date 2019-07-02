9a70eef051844776c604484b3f6a8c8964f2b73c
Mehmet Sezer <mehmet_sezer@hacettepe.edu.tr>
2019-07-02 17:34:45 +0300

    code is pushed

---
+++ publicrepoviewer.py
@@ -0,0 +1,37 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+def printAllRepo(x):
+	for i in x:
+		if(i.get("itemprop") == "name codeRepository"):
+			i = i.text
+			i=i.strip()
+			i=i.replace("\t","")
+			print(i)
+
+
+githubprofilename = input("Enter github profile name: ")
+
+url = "https://github.com/"+githubprofilename+"?tab=repositories"
+nextControl = False
+while(nextControl == False):
+	response = requests.get(url)
+
+	html_icerigi = response.content
+
+	soup = BeautifulSoup(html_icerigi,"html.parser")
+
+	x = soup.find_all("a")
+	
+	printAllRepo(x)
+	nextPage = 0
+	for i in soup.find_all("a"):
+		if(i.text == "Next"):
+			nextPage = 1
+			url=i.get("href")
+			
+	if(nextPage == 0):
+		break
+	
+		
